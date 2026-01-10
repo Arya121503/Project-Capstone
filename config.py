43bf734d4ed1a3947e9ce0346b2a5e8e0f8302fe
Zulfa Azka F <zulfaazka354@yahoo.com>
2026-01-10 12:15:42 +0700

    Prepare for deployment

--- config.py
+++ config.py
@@ -1,26 +1,13 @@
 import os
 from dotenv import load_dotenv
-from db_config import get_database_config
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Get database configuration
-db_config = get_database_config()
-
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'telkom-dashboard-secret'
-    
-    # Database configuration - supports both MySQL and PostgreSQL
-    MYSQL_HOST = db_config['host']
-    MYSQL_PORT = db_config['port']
-    MYSQL_USER = db_config['user']
-    MYSQL_PASSWORD = db_config['password']
-    MYSQL_DB = db_config['database']
-    
-    # SQLAlchemy URI
-    SQLALCHEMY_DATABASE_URI = db_config['sqlalchemy_uri']
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Database type flag
-    DB_TYPE = db_config['type']
+    MYSQL_HOST = os.environ.get('MYSQL_HOST') or 'localhost'
+    MYSQL_PORT = os.environ.get('MYSQL_PORT') or 3306
+    MYSQL_USER = os.environ.get('MYSQL_USER') or 'root'
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or ''
+    MYSQL_DB = os.environ.get('MYSQL_DB') or 'db_kp'
